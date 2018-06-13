cc4ace4177f33e0c57f16edb68b5381278296485
kramerer <maksf77@gmail.com>
2018-06-13 11:45:11 -0700

    sdwetwet

---
+++ src/ua/od/hillel/maksym_fedorovych/FirstTask.java
@@ -0,0 +1,10 @@
+package ua.od.hillel;
+
+public class FirstTask {
+    public static void main(String[] args) {
+        String s1 = "1lesson1lesson1lesson";
+        s1 = s1.replaceAll("1", "2");
+        s1 = s1.replaceAll("n", "n ");
+        System.out.println(s1);
+    }
+}
