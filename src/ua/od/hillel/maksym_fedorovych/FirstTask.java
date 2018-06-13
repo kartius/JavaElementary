ebeb30caa4d51fe31b389facd05808767296e69f
kramerer <maksf77@gmail.com>
2018-06-13 12:10:29 -0700

    rrr

--- src/ua/od/hillel/maksym_fedorovych/FirstTask.java
+++ src/ua/od/hillel/maksym_fedorovych/FirstTask.java
@@ -2,7 +2,7 @@ package ua.od.hillel;
 
 public class FirstTask {
     public static void main(String[] args) {
-        /*xxc */
+        /*xx c*/
         String s1 = "1lesson1lesson1lesson";
         s1 = s1.replaceAll("1", "2");
         s1 = s1.replaceAll("n", "n ");
