0dea37d3e2f6f4b7bb99c747256e0f84b439b1e2
Blackbackroom <orwell184>
2018-06-15 22:21:27 +0300

    lesson3 factorial and fibonachi

--- src/ua/od/hillel/blackbackroom/src/lesson3/lesson3.java
+++ src/ua/od/hillel/blackbackroom/src/lesson3/lesson3.java
@@ -1,4 +1,19 @@
 package lesson3;
-
+import java.util.Scanner;
 public class lesson3 {
+    public static void main(String[] args) {
+        //Сканер значения факториала
+        System.out.println("Введите значение факториала");
+        Scanner scanner=new Scanner(System.in);
+        int end=scanner.nextInt();
+        //Начальное значение факториала
+        int fact=1;
+        //Цикл для расчёта факториала
+        for(int i=1;i<end;i++){
+            fact*=(i+1);
+        }
+        //Вывод на экран результата
+        System.out.println("Факториал "+end+" равен: "+fact);
+    }
+    
 }
