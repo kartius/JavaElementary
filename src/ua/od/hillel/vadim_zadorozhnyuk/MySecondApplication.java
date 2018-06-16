ab96c7223d513d7a8eed1d2d22e14f65249ff861
Vadim Z <40147178+vadooss1@users.noreply.github.com>
2018-06-16 23:22:23 +0300

    The tasks of a lesson2 are done

--- src/ua/od/hillel/vadim_zadorozhnyuk/MySecondApplication.java
+++ src/ua/od/hillel/vadim_zadorozhnyuk/MySecondApplication.java
@@ -1,5 +1,31 @@
 package ua.od.hillel.vadim_zadorozhnyuk;
 
 public class MySecondApplication {
-    String n = "1lesson1lesson1lesson";
+    public static void main(String[] args) {
+        //task1
+        int myWeight = 80;
+        double moonIndex = 0.17;
+        System.out.println("My weight on moon is " + myWeight*moonIndex + " kg");
+
+        //task2
+        String n = "1lesson1lesson1lesson";
+        n=n.replace("1", "2");
+        StringBuffer result = new StringBuffer(n);
+        int k=1;
+        for (int i = 0; i < n.length(); i++) {
+            if(n.charAt(i)=='n'){
+                result.insert(i+k, " ");
+               k++;
+            }
+
+        }
+        System.out.println(result);
+
+        //task 3
+        int from = 1, to = 5;
+        int digit = (int)(Math.random()*(to-from)+from);
+
+        System.out.println(digit);
+    }
+
 }
