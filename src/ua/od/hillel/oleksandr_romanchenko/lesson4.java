cd44c5677a019d0b078df071dc95b4ecc051b61f
Valkoiset <alexandrbelui2@gmail.com>
2018-06-21 12:32:28 +0300

    lesson4

---
+++ src/ua/od/hillel/oleksandr_romanchenko/lesson4.java
@@ -0,0 +1,23 @@
+package ua.od.hillel.oleksandr_romanchenko;
+
+public class lesson4 {
+    public static void main(String[] args) {
+
+        lesson4 check = new lesson4();
+        check.currencyConverter(26.1, "rub");
+    }
+
+    void currencyConverter(double userMoney, String str) {     // uah to usd, eur, rub
+
+        if (str == "usd") {
+            userMoney /= 26.1;
+            System.out.println(userMoney);
+        } else if (str == "eur") {
+            userMoney /= 30.2;
+            System.out.println(userMoney);
+        } else if (str == "rub") {
+            userMoney /= 0.37;
+            System.out.println(userMoney);
+        }
+    }
+}
