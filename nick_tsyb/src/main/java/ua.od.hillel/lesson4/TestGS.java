f9fa2b5a8837dacac22464fb6df8c19b80b03526
kartius <kartius89@gmail.com>
2018-07-01 12:00:37 +0300

    add maven module

---
+++ nick_tsyb/src/main/java/ua.od.hillel/lesson4/TestGS.java
@@ -0,0 +1,22 @@
+package ua.od.hillel.lesson4;
+
+import ua.od.hillel.nick_tsyb.lesson4.application.Calculator;
+
+public class TestGS {
+
+
+    public static void main(String[] args) {
+
+
+        while (true) {
+
+            new Calculator("hghfh");
+
+
+        }
+
+
+    }
+
+
+}
