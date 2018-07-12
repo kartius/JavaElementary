460ac7eb2700545fb37241e566f7eb63d77abdaa
maksym_fedorovych <maksf77@gmail.com>
2018-07-12 19:43:43 +0300

    addmaven

---
+++ lessonabove8/src/main/java/lesson9/AbstractClass.java
@@ -0,0 +1,12 @@
+package lesson9;
+
+public abstract class AbstractClass {
+
+   public void parse(String str){
+       //todo
+       sendStr(str);
+   }
+
+    abstract void sendStr(String str);
+
+}
