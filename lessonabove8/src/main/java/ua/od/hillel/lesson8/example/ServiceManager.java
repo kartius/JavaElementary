697ce525acb38f660db83f03b273fe2d122357a9
kramerer <maksf77@gmail.com>
2018-07-14 15:08:52 +0300

    Push last exhange

--- lessonabove8/src/main/java/ua/od/hillel/lesson8/example/ServiceManager.java
+++ lessonabove8/src/main/java/ua/od/hillel/lesson8/example/ServiceManager.java
@@ -5,9 +5,11 @@ public class ServiceManager {
     private boolean isDataBaseExists;
 
 
-    public DataService getDataService(){
+    public ua.od.hillel.lesson8.example.DataService getDataService(){
       if (!isDataBaseExists) {
-          return new MockDataService();
+          System.out.println("ppp1");
+          return new ua.od.hillel.lesson8.example.MockDataService();
+
       }
       return null;
     }
