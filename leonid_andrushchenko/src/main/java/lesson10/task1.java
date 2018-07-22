227c57ac65e50f44a8b467ba35a62b675abbded1
Leonid Andrushchenko <andrushchenkoleonid@gmail.com>
2018-07-23 00:21:08 +0300

    Leonid Andrushchenko Maven modul

--- leonid_andrushchenko/src/main/java/lesson10/task1.java
+++ leonid_andrushchenko/src/main/java/lesson10/task1.java
@@ -6,30 +6,4 @@ public class task1 {
         str = str.replaceAll("\\.", "[dot]").replaceAll("@", "[ at ]");
         System.out.println(str);
     }
-
-
-
-    public static String abridge(String str){
-        char[] str_chars = str.toCharArray();
-        if (str_chars.length < 4){
-            return str;
-        }
-
-
-        StringBuilder stringBuilder = new StringBuilder();
-
-
-        char[] middleSize = Integer.toString(str_chars.length - 2).toCharArray();// Вычисляет колличество букв между первой и последней, и превращает его в массив символов
-        stringBuilder.setLength(middleSize.length + 2);
-
-        stringBuilder.setCharAt(0, str_chars[0]);
-
-        for(int i = 0; i < middleSize.length; i++){// Копирует число букв между первой и последней в слове посимвольно
-            stringBuilder.setCharAt(i+1, middleSize[i]);
-        }
-
-        stringBuilder.setCharAt(stringBuilder.length() - 1, str_chars[str_chars.length - 1]);//  копирует последнюю букву в слове
-
-        return stringBuilder.toString();
-    }
 }
