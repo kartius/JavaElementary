db3f875af2af478c0e86fe8da1c7f8e4fb0f0c93
Vadim Z <40147178+vadooss1@users.noreply.github.com>
2018-07-23 11:45:15 +0300

    I change the loop on the method replace in the task1

--- src/ua/od/hillel/vadim_zadorozhnyuk/taskStrings/Task1.java
+++ src/ua/od/hillel/vadim_zadorozhnyuk/taskStrings/Task1.java
@@ -3,17 +3,8 @@ package ua.od.hillel.vadim_zadorozhnyuk.taskStrings;
 public class Task1 {
     public static void main(String[] args) {
         String str1 = "person@gmail.com";
-        String result = "";
-
-        for (char i : str1.toCharArray()) {
-            if (i == '@') {
-                result += "[ at ]";
-            } else if (i == '.') {
-                result += "[ dot ]";
-            } else {
-                result += i;
-            }
-        }
+        String result = str1.replace("@", "[ at ]");
+        result = result.replace(".", "[ dot ]");
 
         System.out.println(result);
     }
