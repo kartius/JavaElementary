b1fe7022fead43f1fed544dbd39fe4b6aa1f85aa
Vadim Z <40147178+vadooss1@users.noreply.github.com>
2018-08-07 11:37:30 +0300

    There is task hotel...

---
+++ src/ua/od/hillel/vadim_zadorozhnyuk/task_Hotel/Resident.java
@@ -0,0 +1,13 @@
+package ua.od.hillel.vadim_zadorozhnyuk.task_Hotel;
+
+import java.util.Objects;
+
+public class Resident {
+
+    private String name;
+
+    public Resident(String name) {
+        this.name = name;
+    }
+
+}
