e5220b151b252288cf29dbbbe8a37f4d60158a76
blackbackroom <orwell1984>
2018-08-12 14:28:16 +0300

    Hotel

---
+++ VictorSaranchuk/src/main/java/lesson15HomeWork/CustomArrayList.java
@@ -0,0 +1,195 @@
+package lesson15HomeWork;
+
+
+import java.util.ListIterator;
+import java.util.NoSuchElementException;
+
+public class CustomArrayList implements CustomList {
+    //constant
+    private final int firstSize=0;
+    String[] array=new String[firstSize];
+
+    // Size of List
+    @Override
+    public int size() {
+        return array.length;
+    }
+
+    // Check for empty List
+    @Override
+    public boolean isEmpty() {
+        boolean c= true;
+       if(array.length>0){
+           int count=0;
+           for (String s:array
+                ) {
+               if (s!=null) count++;
+           }
+           if(count>0) c=false;
+       }
+        return c;
+    }
+    // If contains element return true
+    @Override
+    public boolean contains(String element) {
+        int count=0;
+        boolean c=false;
+        for (String s:array
+             ) {
+            if(s==element) count++;
+        } if(count>0) c=true;
+        return c;
+    }
+
+    //add element after last position
+    @Override
+    public void add(String element) {
+        changeSize(array.length+1);
+        array[array.length-1]=element;
+    }
+
+    //add element in index position
+    @Override
+    public void add(int index, String element) {
+        String[] newArray=new String[array.length+1];
+        if(index>array.length){changeSize(index+1); array[index]=element;}
+        else if(index==0&&index<=array.length){
+            if(index==0||index==array.length){
+                add(element);
+            }else{
+                System.arraycopy(array,0,newArray,0,index);
+                newArray[index]=element;
+                System.arraycopy(array,index++,newArray,index++,array.length-index);
+            }
+        }
+
+    }
+
+    // Rremove element contains element
+    @Override
+    public boolean remove(String element) {
+        boolean c=false;
+        remove(indexOf(element));
+        c=true;
+        return c;
+
+    }
+
+    // remove by the index
+    @Override
+    public boolean remove(int index) {
+        boolean c=false;
+        if(index>=0&&index<array.length){
+            for(int i=index;i<array.length-1;i++){
+             array[i]=array[i+1];
+            }
+            changeSize(array.length-1);
+            c=true;
+        }
+    return c;
+    }
+
+    //clear List
+    @Override
+    public void clear() {
+    changeSize(0);
+    }
+
+    //get element by the index
+    @Override
+    public String get(int index) {
+
+        return array[index];
+    }
+
+    //set element by the index
+    @Override
+    public void set(int index, String element) {
+        array[index]=element;
+    }
+
+    //check index of element
+    @Override
+    public int indexOf(String element) {
+        int c=-1;
+        if(size()>0) {
+            for (int i = 0;i<array.length;i++ ){
+                if(array[i]==element) c=i;
+            }
+
+
+        }return c;
+    }
+
+
+    //Help method
+
+    private void changeSize(int newSize){
+        String[] newArray=new String[newSize];
+        if(newSize<0) {changeSize(0);}
+        else if(newSize==0) {array=newArray;}
+        else if(newSize>array.length){
+        System.arraycopy(array,0,newArray,0,array.length);
+        array=newArray;}
+        else{System.arraycopy(array,0,newArray,0, newSize);
+        array=newArray;}
+    }
+
+
+
+
+    class CustomArrayListIterator implements ListIterator {
+        private int cursor;
+        private String[] array;
+
+        CustomArrayListIterator(String[] newArray){
+            this.array=newArray;
+            this.cursor=newArray.length;
+        }
+
+        @Override
+        public boolean hasNext() { return cursor!=size(); }
+
+        @Override
+        public String next() {
+            return null;
+        }
+
+        @Override
+        public boolean hasPrevious() {
+            return cursor!=0;
+        }
+
+        @Override
+        public String previous() {
+            if(cursor==0) throw new NoSuchElementException();
+
+            return array[cursor--];
+        }
+
+        @Override
+        public int nextIndex() {
+            return 0;
+        }
+
+        @Override
+        public int previousIndex() {
+            return 0;
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public void set(Object o) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public void add(Object o) {
+            throw new UnsupportedOperationException();
+        }
+    }
+}
---
+++ VictorSaranchuk/src/main/java/lesson15HomeWork/Runner.java
@@ -0,0 +1,46 @@
+package lesson15HomeWork;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Runner {
+    public static void main(String[] args) {
+        CustomArrayList customArrayList=new CustomArrayList();
+
+        System.out.println("Check first size of list "+customArrayList.size());
+        System.out.println("List is empty "+customArrayList.isEmpty());
+        System.out.println("List contains Ann "+customArrayList.contains("Ann"));
+
+        customArrayList.add(0,"Billy");
+        customArrayList.add("Cindy");
+        customArrayList.add("Dany");
+        customArrayList.add("Mark");
+        customArrayList.add(5, "Richard");
+        System.out.println(customArrayList.size());
+        System.out.println(customArrayList.get(1));
+        customArrayList.remove(1);
+        System.out.println(customArrayList.get(1));
+        System.out.println(customArrayList.indexOf("Dany"));
+
+
+
+        for (int i=0; i<customArrayList.size();i++){
+            System.out.print(customArrayList.get(i)+" ");
+        }
+        System.out.println();
+
+        customArrayList.remove("Mark");
+
+        for (int i=0; i<customArrayList.size();i++){
+            System.out.print(customArrayList.get(i)+" ");
+        }
+
+        System.out.println();
+
+        customArrayList.clear();
+        System.out.println(customArrayList.size());
+
+
+    }
+
+}
