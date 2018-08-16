f18a358959ec49ecd5a295ffde416bdd54036a28
Dana Kirilyuk <danakirilyuk@gmail.com>
2018-08-16 12:22:47 +0300

    add homework HashCode (guest surnames) fixed GuestFull class

--- DanaKirilyuk/src/main/java/HashCode/GuestFull.java
+++ DanaKirilyuk/src/main/java/HashCode/GuestFull.java
@@ -8,5 +8,18 @@ public class GuestFull {
         this.name = name;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GuestFull guestFull = (GuestFull) o;
+        return name != null ? name.equals(guestFull.name) : guestFull.name == null;
+    }
 
+    @Override
+    public int hashCode() {
+        String c = name;
+
+        return name != null ? c.hashCode() : 0;
+    }
 }
